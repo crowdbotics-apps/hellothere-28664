3f21d649abf3ba1a82b3aaa1a69fa264d7c585ea
Crowdbotics <team@crowdbotics.com>
2021-07-09 15:47:06 +0000

    Update models

--- backend/hellothere_28664/wsgi.py
+++ backend/hellothere_28664/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hellothere_28664.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellothere_28664.settings")
 
 application = get_wsgi_application()
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,21 @@
+from django.conf import settings
 from django.db import models
+
+
+class Comedians(models.Model):
+    "Generated Model"
+    name = models.CharField(
+        max_length=256,
+    )
+
+
+class Jokes(models.Model):
+    "Generated Model"
+    joke = models.CharField(
+        max_length=256,
+    )
+    comedian = models.ForeignKey(
+        "home.Comedians",
+        on_delete=models.PROTECT,
+        related_name="jokes_comedian",
+    )
